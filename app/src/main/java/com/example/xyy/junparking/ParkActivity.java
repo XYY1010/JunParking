7b9e0e50dbcb9697b164bf5180524ee26bc63ad6
xyy1010 <742046560@qq.com>
2018-04-13 16:39:26 +0800

    modify3

--- app/src/main/java/com/example/xyy/junparking/ParkActivity.java
+++ app/src/main/java/com/example/xyy/junparking/ParkActivity.java
@@ -1,5 +1,6 @@
 package com.example.xyy.junparking;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -9,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
 
 
 public class ParkActivity extends AppCompatActivity {
@@ -23,11 +25,36 @@ public class ParkActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
         }
+
+        navView.setCheckedItem(R.id.nav_pocket);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item){
+                switch (item.getItemId()){
+                    case R.id.nav_pocket:
+                        Toast.makeText(ParkActivity.this, "You checked your pocket", Toast.LENGTH_SHORT);
+                        break;
+                    case R.id.nav_wallet:
+                        Toast.makeText(ParkActivity.this, "You checked your wallet", Toast.LENGTH_SHORT);
+                        break;
+                    case R.id.nav_setting:
+                        Toast.makeText(ParkActivity.this, "You checked setting", Toast.LENGTH_SHORT);
+                        break;
+                    case R.id.nav_service:
+                        Toast.makeText(ParkActivity.this, "You checked service", Toast.LENGTH_SHORT);
+                        break;
+                    default:
+                }
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
